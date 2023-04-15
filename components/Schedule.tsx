930063c384b72c02f626dccf77a13f09fbcecf56
Jaden Hou <jadenhou19@gmail.com>
2023-04-14 22:54:05 -0400

    schedule

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -3,7 +3,7 @@ import schedule from "../public/schedule.png";
 
 export default function Schedule() {
     return (
-        <section className="w-full bg-slate-900 py-16">
+        <section className="w-full bg-slate-900 py-16" id="schedule">
             <div className="w-4/5 max-w-6xl mx-auto flex flex-col gap-4 items-start">
                 <Image src={schedule} alt="schedule"/>
             </div>
